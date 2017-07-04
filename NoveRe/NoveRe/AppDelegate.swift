76e3a7660d930449c6866f51bdd34c657ea8ea54
Taillook <jousoku@gmail.com>
2017-07-05 01:53:13 +0900

    [Add] setUpAppearance

--- NoveRe/NoveRe/AppDelegate.swift
+++ NoveRe/NoveRe/AppDelegate.swift
@@ -16,16 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        setUpAppearance()
+        return true
+    }
+
+    func setUpAppearance() {
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().barTintColor = UIColor.hex(hexStr: "#EEEEEE", alpha: 1.0)//NavBar背景色
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.black]
         UINavigationBar.appearance().tintColor = UIColor.hex(hexStr: "555555", alpha: 1.0)
-        
-        return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
