7467d5e1fec284d0754aa5de7b8dbb62c994722b
Taillook <jousoku@gmail.com>
2017-07-12 23:19:40 +0900

    [Add] add searchButton on HomeView

--- NoveRe/NoveRe/src/controller/NobelInfoViewController.swift
+++ NoveRe/NoveRe/src/controller/NobelInfoViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class NobelInfoViewController: UIViewController {
-
     var ncode:String = ""
     let webView = UIWebView()
     
@@ -20,7 +19,6 @@ class NobelInfoViewController: UIViewController {
         let back = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(NobelInfoViewController.tapped))
         self.navigationItem.leftBarButtonItem  = back
         let favoriteURL = NSURL(string: "http://ncode.syosetu.com/" + ncode)
-        print(ncode)
         let urlRequest = NSURLRequest(url: favoriteURL! as URL)
         webView.frame = CGRect(x:0,y:0,width:view.bounds.width,height:view.bounds.height)
         webView.loadRequest(urlRequest as URLRequest)
